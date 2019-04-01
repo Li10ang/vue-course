fea75cc9e5d92b560ffa6d4721047caefd652803
liangyao <1083312479@qq.com>
2019-04-01 08:30:19 +0800

    day2

---
+++ day2/my-app/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <div>
+      <input type="text" v-model="info">
+      <button @click="handleClick">添加</button>
+    </div>
+    <ul>
+      <todo-item v-for="item in list" :key="item" >
+        <template v-slot:item="itemProps">
+          {{itemProps}}
+          <span :style="{fontSize: '20px', color: itemProps.checked ? 'red' : 'blue'}">{{item}}</span>
+        </template>
+      </todo-item>
+    </ul>
+  </div>
+</template>
+
+<script>
+import TodoItem from "./components/TodoItem.vue";
+
+export default {
+  name: "app",
+  components: {
+    TodoItem
+  },
+  data() {
+      return {
+          info: '',
+          list: [],
+      }
+  },
+  methods: {
+      handleClick(){
+          this.list.push(this.info);
+          this.info = '';
+      }
+  },
+}
+</script>
+
+<style>
+
+</style>
